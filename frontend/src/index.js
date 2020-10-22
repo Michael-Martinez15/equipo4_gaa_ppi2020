0b4d3ae7e8f9c9ae0e1de2053bde44bc8bcfa0b6
Michael Martinez Jaramillo <66487199+Michael-Martinez15@users.noreply.github.com>
2020-10-21 19:20:23 -0500

    Ppicontainer.js Importamos Ppicontainer en el index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
+import Ppicontainer from "../src/container/Ppicontainer";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <Ppicontainer />,
+    
   document.getElementById('root')
 );
 

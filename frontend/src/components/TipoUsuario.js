df6478a1a5009e3132aa495e364859e04f9bbdf8
Michael Martinez Jaramillo <66487199+Michael-Martinez15@users.noreply.github.com>
2020-10-21 22:15:15 -0500

    TipoUsuario.js Creamos el componente

--- frontend/src/components/TipoUsuario.js
+++ frontend/src/components/TipoUsuario.js
@@ -1,56 +1,40 @@
 import React from 'react';
 import { Button } from 'react-bootstrap'
+
 import { withRouter } from "react-router-dom";
-import "../style/Sugerencias.css";
+import "../style/TipoUsuario.css";
 
 
-const Sugerencias = (props) => {
+const TipoUsuario = (props) => {
   const { history } = props;
-  return (
-    <div className="Sugerencias">
-      <h1 className="TituloSuge"> 
-      <Button className="FlechaAtras btn-light" onClick={() => history.push("/InicioPrincipal")}>
-          <img className="img" src="../img/FlechaA.png" alt="" />
-        </Button>{" "} 
-        Sugerencias</h1>
-      
-      <div className="Prin">
-        <h1 className="Elte"> Deja tu sugerencia o comentario aquí </h1>
-        <img className="Flechis" src="../img/Flechis.png" alt="" /> 
-      </div>
-      
-      
-      <div className="Come">
-        <label for="inputSuge" className=" Comen"> </label>
-        <input type="Suco" id="inputSuge" className="form-control2" placeholder="Ej:la aplicación me gusta. " required="" autofocus="" />
-      </div>
-      
-      <div className="Manis">
-        <h1 className="Let"> ¡ CALIFÍCANOS ! </h1>
-      </div>
-        
-      <div className="Manos">
-          <Button className="Mani1 btn btn-light">
-            <img className="Megus"
-              src="./img/Meg.jpeg"
-              alt="Me gusta"/>
-          </Button>{' '}
-          
-          <Button className="Mani2 btn btn-light">
-           <img className="Megus"
-             src="./img/Nom.jpeg"
-             alt="No me gusta"/>
-          </Button>{' '}
-      </div>
 
-      <div className="Espacio9">
-        <Button className="Enviar btn btn-light" onClick={() => history.push('/Gracias')}>  
-             <h5> Enviar </h5>
+  return (
+    <div className="TipoUsuario">
+
+      <h1 className="UsuarioT">¿Qué usuario</h1>
+      <h1 className="UsuarioT">representas?</h1>
+       
+       <div className="Espacio">
+
+        <Button className="MujerG btn btn-light" onClick={() => history.push('/ComoMujer')}>
+           <img className="Mujer"
+             src="./img/MujerGestante.png"
+             alt="Mujer Gestante"/>
+             Mujer Gestante  
         </Button>{' '}
       </div>
-</div>
-)
+      
+      <div className="Espacio1">
+        <Button className="MujerG btn btn-light" onClick={() => history.push('/ComoObstetra')}>  
+          <img className="Obstetra"
+             src="./img/Obstetra.png"
+             alt="Mujer Gestante"/>
+             Obstetra 
+        </Button>{' '}
+      </div>
+
+    </div>
+ )
 };
 
-
-export default withRouter (Sugerencias);
+export default withRouter (TipoUsuario);

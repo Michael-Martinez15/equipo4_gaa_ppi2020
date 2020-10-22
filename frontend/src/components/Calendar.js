559dda4f76627228df5252601501d0aa2debd900
Sara Yulieth Soto Mesa <66498140+Sara-Mesa13@users.noreply.github.com>
2020-10-21 20:56:18 -0500

    Calendar.js Creamos el componente

---
+++ frontend/src/components/Calendar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+import Ppicontainer from "../src/containers/Ppicontainer";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+ 
+    <Ppicontainer />,
+  rootElement
+);

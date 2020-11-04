a830189de043f9b68cb38f3e0c8e8cd30eaf792b
Michael-Martinez15 <martinezj.150403@gmail.com>
2020-11-04 14:29:41 -0500

    Backend

--- backend/routes/calendario.js
+++ backend/routes/calendario.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const mysqlConnection = require('../db/db');
 
-
+//Esto es una prueba corregir
+router.get('/calendario', (req, res) => {
+    mysqlConnection.query('SELECT * FROM actores', (err, rows, fiedls) => {
+        //Callback
+        //Si no hay error
+     if(!err){ 
+        //Verdadero
+       res.json(rows);
+    }else{
+       console.log(err);
+    }
+    })
+});//Fin get/Estudiantes
 
 module.exports = router;

6bc6735feb87e47162d7b83320295678335990bc
Anderson-Diaz <66498287+Anderson-Diaz@users.noreply.github.com>
2020-11-05 16:59:21 -0500

    index.js creamos el index

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/routes');
-const modulos = require('./routes/modulos');
-const colegios = require('./routes/colegios');
+const calendario = require('./routes/calendario');
+const calificacion = require('./routes/calificacion');
+const control_mensual = require('./routes/control_mensual');
+const obstetra = require('./routes/obstetra');
+const recordatorio = require('./routes/recordatorio');
+const tipo = require('./routes/tipo');
+const usuario = require('./routes/usuario');
 
 //Ajustes
 app.set('port', 3000);
@@ -11,9 +15,14 @@ app.set('port', 3000);
 app.use(express.json());
 
 //Ajustes
-app.use('/api', routes);
-app.use('/api/modulos', modulos);
-app.use('/api/colegios', colegios);
+app.use('/api/calendario', calendario);
+app.use('/api/calificacion', calificacion);
+app.use('/api/control_mensual', control_mensual);
+app.use('/api/obstetra', obstetra);
+app.use('/api/recordatorio', recordatorio);
+app.use('/api/tipo', tipo);
+app.use('/api/usuario', usuario);
+
 
 app.listen(app.get('port'), () => {
     console.log('servidor corriendo en puerto ' + app.get('port'));

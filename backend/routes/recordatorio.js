a88558d5b757b8c58635c2fd4faed892e52331de
Michael Martinez Jaramillo <66487199+Michael-Martinez15@users.noreply.github.com>
2020-11-12 17:25:22 -0500

    recordatorio.js Corregimos el recordatorio

--- backend/routes/recordatorio.js
+++ backend/routes/recordatorio.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+const {Router} = require('express');
+const router = Router();
 const mysqlConnection = require('../db/db');
 
 //Metodo GET

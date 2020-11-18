041120e5265a07192687c278123965a5357d0dad
Sara Yulieth Soto Mesa <66498140+Sara-Mesa13@users.noreply.github.com>
2020-11-18 15:33:13 -0500

    calificaciones.js Cambiamos los códigos de la calificación

--- backend/routes/calificacion.js
+++ backend/routes/calificacion.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const mysqlConnection = require('../db/db');
 
-//Metodo GET
+//Metodo GET - Permite mostrar las calificaciones 
 router.get('/calificacion', (req, res) => {
   mysqlConnection.query('SELECT * FROM calificacion', (err, rows, fiedls) => {
       //Si no hay error
@@ -15,25 +15,22 @@ router.get('/calificacion', (req, res) => {
   })
 });
 
-//Metodo POST
+//Metodo POST - Permite enviar una calificacion 
 router.post('/calificacion', (req, res) => {
-  const{sugerencias,calificacion,id_usuario} = req.body;
-  let Calificacion = [sugerencias,calificacion,id_usuario];
-  let nuevoCalificacion = `INSERT INTO calificacion (sugerencias,calificacion,id_usuario) VALUES (?,?,?)`;
+  const{sugerencias, calificacion, id_usuario} = req.body;
+
+  let Calificacion = [sugerencias, calificacion, id_usuario];
+  let nuevoCalificacion = `INSERT INTO calificacion (sugerencias, calificacion, id_usuario) VALUES (?,?,?);`
   
   
   mysqlConnection.query(nuevoCalificacion, Calificacion, (err, results, fiedls) => {
       if(err){
           return console.error(err.message);
       }else{
-          res.json({message:'Calificacion Ingresada'});
+          res.json({message:'Calificacion Enviada'});
       }
  });
 });
-
-
- 
-
 
 
 module.exports = router;

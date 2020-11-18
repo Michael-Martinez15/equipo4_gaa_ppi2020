14b504773796f89efa71684763e075469719abac
Sara Yulieth Soto Mesa <66498140+Sara-Mesa13@users.noreply.github.com>
2020-11-18 15:28:48 -0500

    recordatorio.js Cambiamos el código del recordatorio

--- backend/routes/recordatorio.js
+++ backend/routes/recordatorio.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const mysqlConnection = require('../db/db');
 
-//Metodo GET
+//Metodo GET  - Permite mostrar los recordatorios 
 router.get('/recordatorio', (req, res) => {
     mysqlConnection.query('SELECT * FROM recordatorio', (err, rows, fiedls) => {
         //Si no hay error
@@ -15,13 +15,13 @@ router.get('/recordatorio', (req, res) => {
     })
 });
 
-//Metodo POST
+//Metodo POST - Permite enviar un nuevo recordatorio 
 router.post('/recordatorio', (req, res) => {
-    const{id, hora, fecha, id_calendario} = req.body;
+    const{id, fecha, id_calendario} = req.body;
 
-    let recordatorio = [id, hora, fecha, id_calendario];
+    let recordatorio = [id, fecha, id_calendario];
     
-    let nuevoRecordatorio = `INSERT INTO recordatorio (id, hora, fecha, id_calendario) VALUES (?,?,?,?)`;
+    let nuevoRecordatorio = `INSERT INTO recordatorio (id, fecha, id_calendario) VALUES (?,?,?);`
     
     mysqlConnection.query(nuevoRecordatorio, recordatorio, (err, results, fiedls) => {
         if(err){
@@ -32,14 +32,14 @@ router.post('/recordatorio', (req, res) => {
    });
 });
 
-//Metodo PUT
+//Metodo PUT - Permite actualizar el recordatorio  por medio del id
 router.put('/recordatorio/:id', (req, res) =>{
-    const{hora, fecha, id_calendario} = req.body;
+    const{fecha, id_calendario} = req.body;
     
     const {id} = req.body;
-    mysqlConnection.query(`UPDATE recordatorio SET hora=?, fecha=?, id_calendario=? WHERE id=?`,
-        [hora, fecha, id_calendario, id], (err, rows, fiedls)=>{
-            if(!error){
+    mysqlConnection.query(`UPDATE recordatorio SET fecha=?, id_calendario=? WHERE id=?`,
+        [fecha, id_calendario, id], (err, rows, fiedls)=>{
+            if(!err){
                 res.json({status:'Recordatorio actualizado'})
             }else{
                 console.log(err.message);
@@ -47,7 +47,7 @@ router.put('/recordatorio/:id', (req, res) =>{
         });
 });
 
-//Metodo DELETE
+//Metodo DELETE - Permite eliminar el recordatorio por medio del id
 router.delete('/recordatorio/:id', (req,res) => {
     const { id } = req.params;
     mysqlConnection.query(`DELETE FROM recordatorio WHERE id =?`,[id],(err,rows,fields) => {

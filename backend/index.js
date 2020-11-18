e4b17769cb1ad010bc0fa7b1a2cf189403eefc66
Anderson-Diaz <66498287+Anderson-Diaz@users.noreply.github.com>
2020-11-18 15:45:35 -0500

    index.js Cambiamos el código del index

--- backend/index.js
+++ backend/index.js
@@ -7,20 +7,19 @@ app.use(express.json());
 const calendario = require('./routes/calendario.js');
 const calificacion = require('./routes/calificacion.js');
 const control_mensual = require('./routes/control_mensual.js');
-const obstetra = require('./routes/obstetra.js');
 const recordatorio = require('./routes/recordatorio.js');
-const tipo = require('./routes/tipo.js');
+const tipo_usuario = require('./routes/tipo_usuario.js');
 const usuario = require('./routes/usuario.js');
 app.use(express.urlencoded({extended: false}));
+
 
 
 //Ajustes
 app.use('/api', calendario);
 app.use('/api', calificacion);
 app.use('/api', control_mensual);
-app.use('/api', obstetra);
 app.use('/api', recordatorio);
-app.use('/api', tipo);
+app.use('/api', tipo_usuario);
 app.use('/api', usuario);
 
 

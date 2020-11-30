67e8592bd69e6204ebfe99381aece8a02e245175
Michael Martinez Jaramillo <66487199+Michael-Martinez15@users.noreply.github.com>
2020-11-30 01:19:06 -0500

    calificacion.js

--- backend/routes/calificacion.js
+++ backend/routes/calificacion.js
@@ -33,4 +33,39 @@ router.post('/calificacion', (req, res) => {
 });
 
 
+//Metodo PUT - Permite actualizar la calificación por medio del id
+router.put("/calificacion/:id_usuario", (req, res) => {
+  const { sugerencias, calificacion } = req.body;
+
+  const { id_usuario } = req.params;
+  mysqlConnection.query(
+    `UPDATE calificacion SET sugerencias=?, calificacion=? WHERE id_usuario=?`,
+    [sugerencias, calificacion, id_usuario],
+    (err, rows, fiedls) => {
+      if (!err) {
+        res.json({ status: "Calificacion actualizada" });
+      } else {
+        console.log(err.message);
+      } //Fin si
+    }
+  );
+});
+
+//Metodo DELETE - Permite eliminar la calificación por medio del id
+router.delete("/calificacion/:id_usuario", (req, res) => {
+  const { id_usuario } = req.params;
+  mysqlConnection.query(
+    `DELETE FROM calificacion WHERE id_usuario =?`,
+    [id_usuario],
+    (err, rows, fields) => {
+      if ("!err") {
+        res.json({ status: `La calificacion ha sido eliminada` });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+
 module.exports = router;
